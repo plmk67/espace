fe73845aff83e9cbdb3c0e7bebc09bde4f3ec4cb
Vincent <Yip.vincent@gmail.com>
2022-11-23 20:16:22 -0500

    adjust link placement on card

--- src/shared/components/Card.js
+++ src/shared/components/Card.js
@@ -6,40 +6,38 @@ const Card = (props) => {
   const { property } = props;
 
   return (
-    <div className="h-80 flex flex-col justify-between  ">
-      <div className="flex w-full h-2/3">
-        <img
-          className="object-cover w-full rounded-lg"
-          src={property.imageUrl}
-          alt={property.imageAlt}
-        />
-      </div>
-      <div className="h-1/3 ">
-        <div className="pt-4">
-          <div display="flex" alignItems="baseline">
-            <div
-              className="font-light"
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-              ml="2"
-            >
-              {property.beds} beds &bull; {property.baths} baths
+    <Link to={`/places/${property.id}/${property.title.toLowerCase()}`}>
+      <div className="h-80 flex flex-col justify-between  ">
+        <div className="flex w-full h-2/3">
+          <img
+            className="object-cover w-full rounded-lg"
+            src={property.imageUrl}
+            alt={property.imageAlt}
+          />
+        </div>
+        <div className="h-1/3 ">
+          <div className="pt-4">
+            <div display="flex" alignItems="baseline">
+              <div
+                className="font-light"
+                color="gray.500"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+              >
+                {property.beds} beds &bull; {property.baths} baths
+              </div>
             </div>
-          </div>
 
-          <div className="font-bold">
-            <Link to={`/places/${property.id}/${property.title.toLowerCase()}`}>
-              {property.title}
-            </Link>
-          </div>
+            <div className="font-bold">{property.title}</div>
 
-          <div>${property.formattedPrice}.00 / night</div>
+            <div>${property.formattedPrice}.00 / night</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
